be79dd2654deb451b08217af33dffeac7e055961
alirezaharaji <alirezask385@gmail.com>
2026-08-05 03:47:44 +0330

    chore: update package.json and vite.config.js for deployment setup
    - Added predeploy and deploy scripts in package.json for building and deploying to GitHub Pages.
    - Included gh-pages as a dev dependency.
    - Added dayjs as a new dependency.
    - Updated vite.config.js to include path aliasing for easier imports and set base path for deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
 
-export default defineConfig({
+
+export default defineConfig({ 
+  base: './',
   plugins: [
     tailwindcss(),
   ],
-  base: './'
+  resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
 })
